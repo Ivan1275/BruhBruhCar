f469f9894a74d97ca912c6ccc7c07af05436541b
JSadracAB <jsdracab@gmail.com>
2023-02-28 16:36:57 +0000

    Tablas/Modelos/Controladores

---
+++ database/migrations/2023_02_28_160534_create_ratings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user1_id')->unsigned();
+            $table->bigInteger('user2_id')->unsigned();
+            $table->integer('score')->unsigned();
+            $table->text('comment');
+            $table->timestamps();
+
+            $table->foreign('user1_id')->references('id')->on('users');
+            $table->foreign('user2_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
