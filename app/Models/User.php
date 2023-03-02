cfa38a4f7f551c5ea33f905169494d2ba0d5a388
Joel <you@example.com>
2023-03-02 19:49:46 +0000

    Trabajo Clase

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Travel::class);
     }
+
+    public function bookings()
+    {
+        return $this->hasMany(Travel::class);
+    }
 }
